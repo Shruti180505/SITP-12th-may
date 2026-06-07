acab3c41fe300d967b1686152beee2691ef26beb
Shruti180505 <shrutisharma18052005@gmail.com>
2026-06-07 12:12:11 +0530

    update

---
+++ hello.py
@@ -0,0 +1,10 @@
+# print("Hello")
+# dict1={'name':'shruti', 'age':20, 'city':"sharma"}
+# print(dict1)
+# set1={1,2,2,3,3,4}
+# print(set1)
+num=range(1,5)
+for i in num:
+    print(i)
+    a=12
+    
